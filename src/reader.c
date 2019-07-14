ec970127fd723345c5bb7871a8156f4c8a6640a0
Ciaran Morgan <ciaran@it-sparkles.co.uk>
2019-07-14 20:36:32 +0100

    updated build info display

--- src/reader.c
+++ src/reader.c
@@ -35,7 +35,7 @@ int main()
 	strcpy(result, "");
 	length = strlen(result);
 	
-	 printf("reader build "BUILD_VERSION" (built "BUILD_DATE")\n");
+	 printf("reader\n======\n Build "BUILD_VERSION" \nbuilt "BUILD_DATE"\n");
 	 
 	
 	
